fa90012ff9b1c076bd4a086a280e009f1f925701
Jonathan Arnett <jonarnett90@gmail.com>
2014-09-09 22:41:32 -0400

    Updated backend controller to have all basic functionality

--- angular-backend/app/controllers/projects_controller.rb
+++ angular-backend/app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:update, :destroy]
   def index
     respond_to do |format|
       format.json { render :json => Project.all }
@@ -6,25 +7,41 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    project = Project.create(project_params)
+    @project = Project.new(project_params)
 
     respond_to do |format|
-      if project
-        format.json { render :json => project }
+      if @project.save
+        format.json { render json: project }
       else
-
+        format.json { render json: @project.errors, status: :unprocessable_entity }
       end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.json { render json: project }
+      else
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @project.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
   end
 
   private
 
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
   def project_params
-    params.require(:project).permit(:title, :description)
+    params.require(:project).permit(:id, :title, :description)
   end
 end
